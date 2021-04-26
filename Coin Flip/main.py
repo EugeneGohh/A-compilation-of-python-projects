d4c7d7b192df9c7a6d76eeeb711af2b22109536a
Eugene Goh <gzkgt11@gmail.com>
2021-04-26 23:47:46 +0800

    Updated code

--- Coin Flip/main.py
+++ Coin Flip/main.py
@@ -5,8 +5,22 @@ head_count = 0
 tail_count = 0
 
 while int(initial_value) > 0:
-    x = input("Head or Tail? ")
-    print("You choose " + x) 
+    x = input("Head or Tail?\n")
+    print("You choose " + x)
 
     shuffle = random.choice(["Head", "Tail"])
-    print(shuffle)
+    print("Head & Tail output by machine is: " + shuffle)
+
+    if (x == shuffle):
+        print(x + " and " + shuffle + " are the same")
+    else: 
+        print(x + " and " + shuffle + " are not the same")
+
+    if (x ==  shuffle):
+        head_count += 1
+        tail_count += 1
+    else:
+        head_count -= 1
+        tail_count -= 1
+
+    print("Head count is " + str(head_count) + " and " + " ," + "Tail count is " + str(tail_count))
