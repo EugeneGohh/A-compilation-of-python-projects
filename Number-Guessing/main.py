e53f1e1d561c3c6dcc01fa388d0c4c464ee3df84
Eugene Goh <gzkgt11@gmail.com>
2021-05-02 05:03:13 +0800

    added computer guess

--- Number-Guessing/main.py
+++ Number-Guessing/main.py
@@ -1,7 +1,7 @@
 import random
 
 def guess(x):
-    random_num = random.randint(1, x); # Return a random number
+    random_num = random.randint(1, x) # Return a random number
     guess = 0
 
     while guess != random_num:
@@ -13,4 +13,22 @@ def guess(x):
     
     print(f"You have guessed the number {random_num}")
 
-guess(5)
+def comp_guess(x):
+    low = 1
+    high = x
+    reply = ""
+
+    while reply != "T" and low != high: # t = true
+        guess = random.randint(low, high)
+        reply = input(f"Is ${guess} too high (H), too low (L), or true(T)? ").lower()
+
+        if reply == "h":
+            high = guess - 1
+        elif reply == "l":
+            low = guess + 1
+        
+        
+    print(f"The computer guesse your number, {guess}")
+
+# guess(5) 
+# comp_guess(5);
